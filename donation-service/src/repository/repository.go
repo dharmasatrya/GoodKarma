e0a4ef93f2da5cf6344f494fd7dd8bd27589fc7b
Dharnit <85389770+dharmasatrya@users.noreply.github.com>
2025-01-17 16:12:33 +0700

    feat/create-donation

---
+++ donation-service/src/repository/repository.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"context"
+	"goodkarma-donation-service/entity"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type DonationRepository interface {
+	CreateDonation(input entity.Donation) (*entity.Donation, error)
+}
+
+type donationRepository struct {
+	db *mongo.Collection
+}
+
+func NewDonationRepository(db *mongo.Collection) *donationRepository {
+	return &donationRepository{db}
+}
+
+func (r *donationRepository) CreateDonation(input entity.Donation) (*entity.Donation, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	donation := entity.Donation{
+		ID:           input.ID,
+		UserID:       input.UserID,
+		EventID:      input.EventID,
+		Amount:       input.Amount,
+		Status:       input.Status,
+		DonationType: input.DonationType,
+	}
+
+	_, err2 := r.db.InsertOne(ctx, donation)
+	if err2 != nil {
+		return nil, err2
+	}
+
+	return &donation, nil
+}
