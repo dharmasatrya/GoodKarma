274b66cb6275413a3bf84541052daf8fd0e1143c
Dharnit <85389770+dharmasatrya@users.noreply.github.com>
2025-01-18 14:36:02 +0700

    feat/gateway donation

--- donation-service/proto/donation_grpc.pb.go
+++ donation-service/proto/donation_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.5.1
 // - protoc             v5.29.2
-// source: donation.proto
+// source: proto/donation.proto
 
 package proto
 
@@ -231,5 +231,5 @@ var DonationService_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "donation.proto",
+	Metadata: "proto/donation.proto",
 }
